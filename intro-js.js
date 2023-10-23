536a885936508081641d10a23b915553486616a6
Megha Banerjee <megha.banerjee07@gmail.com>
2023-10-24 04:09:14 +0530

    intro video on JS

---
+++ intro-js.js
@@ -0,0 +1,10 @@
+// introduction on editor
+var number = 10;
+var greetings = { hello: "there" };
+console.log(number / 20);
+
+console.log(number / 50);
+
+console.log("your number is: " + number);
+
+console.log(greetings);
